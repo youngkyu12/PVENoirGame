3e966831edd5db29d4186fa53c061d1b91c1b759
SYJ1029 <abg0986@gmail.com>
2025-11-27 22:49:47 +0900

    상용 라이브러리
    LF스택을 이미 제공하고 있음

--- GameServer/GameServer/GameServer.cpp
+++ GameServer/GameServer/GameServer.cpp
@@ -11,24 +11,23 @@
 #include "ThreadManager.h"
 #include "Memory.h"
 #include "Allocator.h"
-#include "LockFreeStack.h"
 
 DECLSPEC_ALIGN(16)
 class Data
 {
 public:
-	SListEntry _entry;
+	SLIST_ENTRY _entry;
 
 	int64 _rand = rand() % 1000;
 };
 
-SListHeader* GHeader;
+SLIST_HEADER* GHeader;
 
 int main()
 {
-	GHeader = new SListHeader();
+	GHeader = new SLIST_HEADER();
 	ASSERT_CRASH((uint64)GHeader % 16 == 0);
-	InitializeHeader(GHeader);
+	::InitializeSListHead(GHeader);
 
 	for (int32 i = 0; i < 3; ++i)
 	{
@@ -39,7 +38,7 @@ int main()
 				Data* data = new Data();
 				ASSERT_CRASH((uint64)GHeader % 16 == 0);
 
-				PushEntrySList(GHeader, (SListEntry*)data);
+				InterlockedPushEntrySList(GHeader, (PSLIST_ENTRY)data);
 				this_thread::sleep_for(10ms);
 			}
 		});
@@ -52,7 +51,7 @@ int main()
 				while (true)
 				{
 					Data* pop = nullptr;
-					pop = (Data*)PopEntrySList(GHeader);
+					pop = (Data*)InterlockedPopEntrySList(GHeader);
 
 					if (pop)
 					{
