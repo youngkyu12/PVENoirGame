e54be1d7bba60b3ca83839a21efeac7bb3b9c95b
SYJ1029 <abg0986@gmail.com>
2025-11-30 01:02:49 +0900

    Object Pool

--- GameServer/GameServer/GameServer.cpp
+++ GameServer/GameServer/GameServer.cpp
@@ -1,14 +1,14 @@
 ﻿#include "pch.h"
 #include <iostream>
-
+#include "CorePch.h"
 #include <atomic>
 #include <mutex>
-#include <thread>
+#include <windows.h>
 #include <future>
-#include <iostream>
-#include <chrono>
-
 #include "ThreadManager.h"
+
+#include <chrono>
+#include "RefCounting.h"
 #include "Memory.h"
 #include "Allocator.h"
 
@@ -18,27 +18,44 @@ public:
 	int32 _hp = rand() % 1000;
 };
 
+class Monster
+{
+public:
+	int64 _id = 0;
+};
+
 int main()
 {
+	Knight* knights[100];
 
-	for (int32 i = 0; i < 5; ++i)
+	for (int32 i = 0; i < 100; i++)
+		knights[i] = ObjectPool<Knight>::Pop();
+
+	for (int32 i = 0; i < 100; i++)
+	{
+		ObjectPool<Knight>::Push(knights[i]);
+		knights[i] = nullptr;
+	}
+
+	shared_ptr<Knight> sptr = ObjectPool<Knight>::MakeShared();
+	shared_ptr<Knight> sptr2 = MakeShared<Knight>();
+
+	for (int32 i = 0; i < 5; i++)
 	{
 		GThreadManager->Launch([]()
-		{
+			{
 				while (true)
 				{
 					Knight* knight = xnew<Knight>();
 
 					cout << knight->_hp << endl;
 
-					this_thread::sleep_for(chrono::milliseconds(10ms));
+					this_thread::sleep_for(10ms);
 
 					xdelete(knight);
 				}
-		});
+			});
 	}
-
-
 
 	GThreadManager->Join();
 }
--- GameServer/ServerCore/MemoryPool.cpp
+++ GameServer/ServerCore/MemoryPool.cpp
@@ -12,47 +12,36 @@ MemoryPool::MemoryPool(int32 allocSize) : _allocSize(allocSize)
 
 MemoryPool::~MemoryPool()
 {
-	while (MemoryHeader* memory = static_cast<MemoryHeader*>(InterlockedPopEntrySList(&_header)))
-	{
+	while (MemoryHeader* memory = static_cast<MemoryHeader*>(::InterlockedPopEntrySList(&_header)))
 		::_aligned_free(memory);
-	}
-
 }
 
 void MemoryPool::Push(MemoryHeader* ptr)
 {
 	ptr->allocSize = 0;
 
-	// Pool에 메모리 반납
 	::InterlockedPushEntrySList(&_header, static_cast<PSLIST_ENTRY>(ptr));
 
-	_allocCount.fetch_sub(1);
+	_useCount.fetch_sub(1);
+	_reserveCount.fetch_add(1);
 }
 
 MemoryHeader* MemoryPool::Pop()
 {
-	MemoryHeader* memory = static_cast<MemoryHeader*>(InterlockedPopEntrySList(&_header));
-
-
-	// Pool에 여분이 있는지?
-	if (memory == nullptr)
-	{
-		// 있으면 하나 꺼내온다
-		memory = reinterpret_cast<MemoryHeader*>(::_aligned_malloc(_allocSize, SLIST_ALIGNMENT));
-	}
-
+	MemoryHeader* memory = static_cast<MemoryHeader*>(::InterlockedPopEntrySList(&_header));
 
 	// 없으면 새로 만들다
 	if (memory == nullptr)
 	{
-		memory = reinterpret_cast<MemoryHeader*>(::malloc(_allocSize));
+		memory = reinterpret_cast<MemoryHeader*>(::_aligned_malloc(_allocSize, SLIST_ALIGNMENT));
 	}
 	else
 	{
 		ASSERT_CRASH(memory->allocSize == 0);
+		_reserveCount.fetch_sub(1);
 	}
 
-	_allocCount.fetch_add(1);
+	_useCount.fetch_add(1);
 
 	return memory;
 }
