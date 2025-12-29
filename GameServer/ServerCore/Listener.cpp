761db3e11ba6cc2e1b3dc78649dd44ccfadc185d
SYJ1029 <abg0986@gmail.com>
2025-12-29 16:37:38 +0900

    session(1)

--- GameServer/ServerCore/Listener.cpp
+++ GameServer/ServerCore/Listener.cpp
@@ -113,10 +113,6 @@ void Listener::ProcessAccept(AcceptEvent* acceptEvent)
 	}
 
 	session->SetNetAddress(NetAddress(sockAddress));
-
-	cout << "Client Connected!" << endl;
-
-	// TODO
-
+	session->ProcessConnect();
 	RegisterAccept(acceptEvent);
 }
