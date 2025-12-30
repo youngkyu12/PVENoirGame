5c6b89847b5b3f03201a56383fc2bc104301f9ab
SYJ1029 <abg0986@gmail.com>
2025-12-31 01:55:26 +0900

    PacketSession

--- GameServer/DummyClient/DummyClient.cpp
+++ GameServer/DummyClient/DummyClient.cpp
@@ -4,9 +4,9 @@
 #include "Service.h"
 #include "Session.h"
 
-char sendData[] = "Hello World";
 
-class ServerSession : public Session
+
+class ServerSession : public PacketSession
 {
 public:
 	~ServerSession()
@@ -16,37 +16,31 @@ public:
 
 	virtual void OnConnected() override
 	{
-		cout << "Connected to Server" << endl;
-		SendBufferRef sendBuffer = GSendBufferManager->Open(4096);
-		::memcpy(sendBuffer->Buffer(), sendData, sizeof(sendData));
-		sendBuffer->Close(sizeof(sendData));
-
-		Send(sendBuffer);
+		//cout << "Connected to Server" << endl;
 
 	}
 
-	virtual int32 OnRecv(BYTE* buffer, int32 len) override
+	virtual int32 OnRecvPacket(BYTE* buffer, int32 len) override
 	{
+		PacketHeader header = *(PacketHeader*)&buffer[0];
+
+		char recvBuffer[4096];
+		::memcpy(recvBuffer, &buffer[4], header.size - sizeof(PacketHeader));
+
 		// Echo
-		cout << "OnRecv Len = " << len << endl;
+		cout << recvBuffer << endl;
 
-		this_thread::sleep_for(1s);
-
-		SendBufferRef sendBuffer = GSendBufferManager->Open(4096);
-		::memcpy(sendBuffer->Buffer(), sendData, sizeof(sendData));
-		sendBuffer->Close(sizeof(sendData));
-		Send(sendBuffer);
 		return len;
 	}
 
 	virtual void OnSend(int32 len) override
 	{
-		cout << "OnSend Len = " << len << endl;
+		//cout << "OnSend Len = " << len << endl;
 	}
 
 	virtual void OnDisconnected() override
 	{
-		cout << "Disconnected" << endl;
+		//cout << "Disconnected" << endl;
 	}
 
 };
@@ -59,7 +53,7 @@ int main()
 		NetAddress(L"127.0.0.1", 7777),
 		MakeShared<IocpCore>(),
 		MakeShared<ServerSession>, // TODO : SessionManager 등
-		5);
+		1000);
 
 	ASSERT_CRASH(service->Start());
 
