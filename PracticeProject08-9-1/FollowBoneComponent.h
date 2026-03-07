45d88bb5bd4a3c5b91a938aa710b8526b737c9ec
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-03-07 17:02:59 +0900

    Scene 책임구조 컴포넌트로 이전
    FollowBoneComponent에 대해 Scene이 역할이 좀 무거워서 옮겼음

--- PracticeProject08-9-1/FollowBoneComponent.h
+++ PracticeProject08-9-1/FollowBoneComponent.h
@@ -10,23 +10,27 @@ class CFollowBoneComponent final : public CComponentT<CFollowBoneComponent>
 public:
     explicit CFollowBoneComponent(CGameObject* owner);
 
-    void Bind(CGameObject* target, int boneIndex, const XMFLOAT4X4& localOffset);
+    void Bind(CGameObject* target, const std::string& boneName, const XMFLOAT4X4& localOffset);
     void Clear();
 
     CGameObject* GetTarget() const { return m_pTarget; }
+    const std::string& GetBoneName() const { return m_boneName; }
     int GetBoneIndex() const { return m_boneIndex; }
     const XMFLOAT4X4& GetLocalOffset() const { return m_localOffset; }
 
-    bool IsBound() const { return (m_pTarget != nullptr) && (m_boneIndex >= 0); }
+    bool IsBound() const { return (m_pTarget != nullptr) && !m_boneName.empty(); }
 
     void SnapNow();
     void OnLateUpdate(float dt) override;
 
 private:
-    bool TryBuildFollowerWorld(XMFLOAT4X4& outWorld) const;
+    bool ResolveBoneIndex();
+    bool TryBuildFollowerWorld(XMFLOAT4X4& outWorld);
 
 private:
     CGameObject* m_pTarget = nullptr;
+    std::string  m_boneName;
     int          m_boneIndex = -1;
+    bool         m_boneResolved = false;
     XMFLOAT4X4   m_localOffset{};
 };
