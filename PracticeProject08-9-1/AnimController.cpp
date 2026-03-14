8423f14bcddf9d9eb1640a61203ca78224d13517
SYJ1029 <abg0986@gmail.com>
2026-03-14 22:33:10 +0900

    애니메이션 동기화(2)
    공격 모션

--- PracticeProject08-9-1/AnimController.cpp
+++ PracticeProject08-9-1/AnimController.cpp
@@ -23,6 +23,35 @@ void CAnimController::Update(float /*dt*/)
     if (!anim)
         anim = m_pOwner->GetAnimator();
 
+#ifdef USING_NETWORK
+
+    // 데모: play만 열심히 하자
+    constexpr float kBlendTime = 0.15f;
+
+    if (m_state != animPrevState) {
+        const char* targetClip = ClipFor(m_state);
+
+
+        if(m_state == EAnimState::Attack)
+        {
+            if (!anim->CrossFade(m_attackClip, kBlendTime, false, m_startTime))
+            {
+                anim->Play(m_attackClip, false, m_startTime);
+            }
+            return;
+		}
+
+
+        if (!anim->CrossFade(targetClip, kBlendTime, true, 0.0f))
+        {
+            
+            anim->Play(targetClip, true, m_startTime);
+        }
+    }
+
+	   
+
+#else
     if (m_attackQueued)
     {
         m_attackQueued = false;
@@ -47,8 +76,6 @@ void CAnimController::Update(float /*dt*/)
         }
     }
 
-#ifndef USING_NETWORK
-
     if (m_state == EAnimState::Attack)
     {
         if (anim->IsCurrentClipFinished())
@@ -71,30 +98,7 @@ void CAnimController::Update(float /*dt*/)
         }
         return;
     }
-#endif 
 
-
-#ifdef USING_NETWORK
-    if (m_state == EAnimState::Attack) {
-        animPrevState = m_state;
-        return;
-    }
-
-    // 데모: play만 열심히 하자
-    constexpr float kBlendTime = 0.15f;
-
-    if (m_state != animPrevState) {
-        const char* targetClip = ClipFor(m_state);
-        if (!anim->CrossFade(targetClip, kBlendTime, true, 0.0f))
-        {
-            
-            anim->Play(targetClip, true, m_startTime);
-        }
-    }
-
-	   
-
-#else
     EAnimState target = (m_speed > m_moveEps) ? EAnimState::Move : EAnimState::Idle;
 
     const char* targetClip = ClipFor(target);
