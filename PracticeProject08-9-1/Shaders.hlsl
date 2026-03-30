fe2609e6f4aec50e174ce83380bc9d342bb60d5d
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-03-30 20:03:56 +0900

    엠비언트 계산 응급처치
    디렉션 라이트의 엠비언트 항만으로 계산. material, m_xmf4GlobalAmbient 완전 무시함

--- PracticeProject08-9-1/Shaders.hlsl
+++ PracticeProject08-9-1/Shaders.hlsl
@@ -349,7 +349,7 @@ PS_MULTIPLE_RENDER_TARGETS_OUTPUT PSTexturedLightingToMultipleRTs(
     output.cTexture = texColor;
     output.cIllumination = illumination;
     output.color = illumination;
-    //output.color = texColor;
+    //output.color = float4(gLights[2].m_cAmbient.rgb * texColor.rgb, 1.0f);
     output.normal = float4(normalW * 0.5f + 0.5f, 1.0f);
     output.zDepth = input.position.z;
 
