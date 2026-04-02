59f902dee3c8bfa32b73e6fecf7e9cb33367b370
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-04-02 15:03:21 +0900

    구르기 시에도 히트되던 버그 픽스

--- PracticeProject08-9-1/WeaponHitboxComponent.h
+++ PracticeProject08-9-1/WeaponHitboxComponent.h
@@ -4,10 +4,9 @@
 #pragma once
 
 #include "Component.h"
-#include "PlayerEquipmentComponent.h"
+#include <unordered_set>
 
-class CPlayerEquipmentComponent;
-class CColliderComponent;
+class CGameObject;
 
 class CWeaponHitboxComponent final : public CComponentT<CWeaponHitboxComponent>
 {
@@ -17,11 +16,15 @@ public:
 	void OnCreate(ID3D12Device* dev, ID3D12GraphicsCommandList* cmd) override;
 	void OnUpdate(float dt) override;
 
-private:
-	void SetWeaponColliderEnabled(EWeaponType type, bool enabled);
-	void DisableAllMeleeWeaponColliders();
-	bool IsMeleeAttackActive() const;
+public:
+	// 한 번 휘두를 때 같은 대상 중복 타격 방지용 캐시를 이미 쓰고 있으면
+	// 그 캐시 clear 함수 이름만 맞춰서 연결하면 된다.
+	void ClearHitTargets() { m_hitTargets.clear(); }
 
 private:
-	CPlayerEquipmentComponent* m_pEquipment = nullptr;
+	void DisableAllWeaponColliders();
+
+private:
+	bool m_bPrevHitboxActive = false;
+	std::unordered_set<CGameObject*> m_hitTargets;
 };
