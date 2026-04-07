abc4fe661540851e071620dcd8d52e19c658c872
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-04-08 01:21:31 +0900

    성/나무 서브메시 가져오는데 나무는 문제 생김

--- PracticeProject08-9-1/GameScene.cpp
+++ PracticeProject08-9-1/GameScene.cpp
@@ -2412,7 +2412,7 @@ void CGameScene::BuildSkinnedBatch(
 
     const UINT fighterCount = m_PlayerCount;
 
-    const XMFLOAT3 playerBase(-150.0f, 0.0f, 0.0f);
+    const XMFLOAT3 playerBase(0.0f, 0.0f, 0.0f);
 
     m_swordManRefs.clear();
     m_swordManRefs.reserve(m_swordManCount);
