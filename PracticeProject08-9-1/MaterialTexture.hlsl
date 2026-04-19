21271a17574d379c64bf9e1c7658ada904beac8a
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-04-19 21:23:05 +0900

    Revert "shadowmap 구조체 수정"
    This reverts commit 2baa9db2df6384e9c686789e2d8639d2cae863f2.

--- PracticeProject08-9-1/MaterialTexture.hlsl
+++ PracticeProject08-9-1/MaterialTexture.hlsl
@@ -139,12 +139,8 @@ float3 GetNormalWFromMap(uint packedNormal, float3 normalW_in, float4 tangentW_i
 //---------------------------------------------------------------------------------------
 float CalcShadowFactor(float4 shadowPosH)
 {
-    float shadowFactor = 1.0f;
-
-    const uint shadowSrvIndex = gShadowMeta.x;
-    if (shadowSrvIndex == INVALID_TEXTURE_INDEX || shadowSrvIndex >= MAX_GLOBAL_SRVS)
-        return shadowFactor;
-
+    /*float shadowFactor = 1.0f;
+    
     if (shadowPosH.w <= 0.0f)
         return shadowFactor;
 
@@ -160,7 +156,7 @@ float CalcShadowFactor(float4 shadowPosH)
     float depth = shadowPosH.z;
 
     uint width, height, numMips;
-    gtxtGlobalTextures[shadowSrvIndex].GetDimensions(0, width, height, numMips);
+    gtxtGlobalTextures[SHADOW_MAP_SRV_INDEX].GetDimensions(0, width, height, numMips);
 
     float dx = 1.0f / (float) width;
 
@@ -176,7 +172,7 @@ float CalcShadowFactor(float4 shadowPosH)
     for (int i = 0; i < 9; ++i)
     {
         const float sampledDepth =
-            gtxtGlobalTextures[shadowSrvIndex].SampleLevel(
+            gtxtGlobalTextures[SHADOW_MAP_SRV_INDEX].SampleLevel(
                 gsamLinearClamp,
                 shadowPosH.xy + offsets[i],
                 0.0f
@@ -185,9 +181,12 @@ float CalcShadowFactor(float4 shadowPosH)
         percentLit += (depth <= sampledDepth + 0.0005f) ? 1.0f : 0.0f;
     }
 
+
     shadowFactor = percentLit / 9.0f;
-    return shadowFactor;
+    return shadowFactor;*/
+    return 1.0f;
 }
+
 float ResolveLinearDepthFromDeviceZ(float deviceZ)
 {
     const float nearZ = max(0.0001f, gvFogParams1.x);
--- PracticeProject08-9-1/ShadowMap.h
+++ PracticeProject08-9-1/ShadowMap.h
@@ -9,11 +9,9 @@ class CGameObject;
 
 struct CB_SHADOW_PASS
 {
-	DirectX::XMFLOAT4X4 ViewProj = Matrix4x4::Identity();
+	DirectX::XMMATRIX ViewProj = {};
 	DirectX::XMFLOAT4X4 ShadowTransform = Matrix4x4::Identity();
-	DirectX::XMFLOAT3 LightDirectionW = { 0.0f, -1.0f, 0.0f };
-	float PadShadow0 = 0.0f;
-	DirectX::XMUINT4 ShadowMeta = DirectX::XMUINT4(UINT_MAX, 0, 0, 0);
+	DirectX::XMFLOAT3 EyePosW = { 0.0f, 0.0f, 0.0f };
 };
 
 class ShadowMap
@@ -52,11 +50,9 @@ public:
 	void BuildDescriptors(
 		D3D12_CPU_DESCRIPTOR_HANDLE hCpuSrv,
 		D3D12_GPU_DESCRIPTOR_HANDLE hGpuSrv,
-		D3D12_CPU_DESCRIPTOR_HANDLE hCpuDsv,
-		UINT shadowSrvIndex);
+		D3D12_CPU_DESCRIPTOR_HANDLE hCpuDsv);
 
 	void OnResize(UINT newWidth, UINT newHeight);
-	UINT GetSrvIndex() const { return m_nShadowSrvIndex; }
 
 private:
 	void BuildDescriptors();
@@ -96,6 +92,4 @@ private:
 	DirectX::XMFLOAT4X4 mLightView = Matrix4x4::Identity();
 	DirectX::XMFLOAT4X4 mLightProj = Matrix4x4::Identity();
 	DirectX::XMFLOAT4X4 mShadowTransform = Matrix4x4::Identity();
-	
-	UINT m_nShadowSrvIndex = UINT_MAX;
 };
