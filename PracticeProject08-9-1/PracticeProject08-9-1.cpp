cf730d931ea07b0b0955b3e73a040d98a9fe04fb
SYJ1029 <abg0986@gmail.com>
2026-04-19 14:12:28 +0900

    여전히 CRASH

--- PracticeProject08-9-1/PracticeProject08-9-1.cpp
+++ PracticeProject08-9-1/PracticeProject08-9-1.cpp
@@ -59,7 +59,9 @@ public:
 
 	virtual void OnDisconnected() override
 	{
-		//cout << "Disconnected" << endl;
+		//cout << "Disconnected" << endl
+		// 연결을 끊을 때 해당 세션을 정리한다
+		LSendBufferChunk->Reset();
 	}
 };
 
@@ -103,8 +105,11 @@ int APIENTRY _tWinMain(
 	{
 		if (::PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
-			if (msg.message == WM_QUIT) 
+			if (msg.message == WM_QUIT)
+			{
+				g_End.store(true);
 				break;
+			}
 			if (!::TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
 			{
 				::TranslateMessage(&msg);
@@ -118,6 +123,13 @@ int APIENTRY _tWinMain(
 	}
 	gGameFramework.OnDestroy();
 	GThreadManager->Join();
+
+	// ESC 키가 눌렸다면 네트워크 루프 종료
+	if ( g_clientService )
+	{
+		g_clientService->CloseService();
+		g_clientService.reset();
+	}
 
 	
 	return((int)msg.wParam);
@@ -208,8 +220,7 @@ void NetworkLoop()
 					g_clientService->GetIocpCore()->Dispatch(100);
 					if (CheckEnd())
 					{
-						// ESC 키가 눌렸다면 네트워크 루프 종료
-						
+						g_clientService->CloseService();
 						break;
 					}
 
@@ -268,6 +279,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
+		g_End = false;
 		::PostQuitMessage(0);
 		break;
 	default:
