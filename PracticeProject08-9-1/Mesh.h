57f865cbd984b6c202c9fa8354f934e94b1ca565
youngkyu12 <112385423+youngkyu12@users.noreply.github.com>
2026-04-23 17:15:11 +0900

    Terrain 준비

--- PracticeProject08-9-1/Mesh.h
+++ PracticeProject08-9-1/Mesh.h
@@ -224,3 +224,57 @@ public:
 	CCapsuleMeshDiffused(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, CColliderComponent* Collider);
 	virtual ~CCapsuleMeshDiffused() {}
 };
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+class CHeightMapImage
+{
+private:
+	BYTE* m_pHeightMapPixels;
+
+	int							m_nWidth;
+	int							m_nLength;
+	XMFLOAT3					m_xmf3Scale;
+
+public:
+	CHeightMapImage(LPCTSTR pFileName, int nWidth, int nLength, XMFLOAT3 xmf3Scale);
+	~CHeightMapImage(void);
+
+	float GetHeight(float x, float z, bool bReverseQuad = false);
+	XMFLOAT3 GetHeightMapNormal(int x, int z);
+	XMFLOAT3 GetScale() { return( m_xmf3Scale ); }
+
+	BYTE* GetHeightMapPixels() { return( m_pHeightMapPixels ); }
+	int GetHeightMapWidth() { return( m_nWidth ); }
+	int GetHeightMapLength() { return( m_nLength ); }
+};
+
+class CGridMesh : public CMesh
+{
+protected:
+	int							m_nWidth;
+	int							m_nLength;
+	XMFLOAT3					m_xmf3Scale = XMFLOAT3(1.0f, 1.0f, 1.0f);
+
+public:
+	CGridMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, int xStart, int zStart, int nWidth, int nLength, XMFLOAT3 xmf3Scale = XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT4 xmf4Color = XMFLOAT4(1.0f, 1.0f, 0.0f, 0.0f), void* pContext = NULL);
+	virtual ~CGridMesh();
+
+	XMFLOAT3 GetScale() { return( m_xmf3Scale ); }
+	int GetWidth() { return( m_nWidth ); }
+	int GetLength() { return( m_nLength ); }
+
+	virtual float OnGetHeight(int x, int z, void* pContext) { return( 0.0f ); }
+	virtual XMFLOAT4 OnGetColor(int x, int z, void* pContext) { return( XMFLOAT4(0.0f, 0.0f, 0.0f, 0.0f) ); }
+};
+
+class CHeightMapGridMesh : public CGridMesh
+{
+public:
+	CHeightMapGridMesh(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, int xStart, int zStart, int nWidth, int nLength, XMFLOAT3 xmf3Scale = XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT4 xmf4Color = XMFLOAT4(1.0f, 1.0f, 0.0f, 0.0f), void* pContext = NULL);
+	virtual ~CHeightMapGridMesh();
+
+	virtual float OnGetHeight(int x, int z, void* pContext);
+	virtual XMFLOAT4 OnGetColor(int x, int z, void* pContext);
+};
+
--- PracticeProject08-9-1/Shader.h
+++ PracticeProject08-9-1/Shader.h
@@ -418,3 +418,52 @@ public:
 
 	void UpdateShaderVariables(ID3D12GraphicsCommandList* cmd, void* pContext) override;
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+class CTerrainShader : public CTexturedShader
+{
+public:
+	CTerrainShader();
+	virtual ~CTerrainShader();
+
+	virtual D3D12_INPUT_LAYOUT_DESC CreateInputLayout();
+
+	virtual D3D12_SHADER_BYTECODE CreateVertexShader(ID3DBlob** ppd3dShaderBlob);
+	virtual D3D12_SHADER_BYTECODE CreatePixelShader(ID3DBlob** ppd3dShaderBlob);
+
+	virtual void CreateShader(ID3D12Device* pd3dDevice, ID3D12RootSignature* pd3dGraphicsRootSignature);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+class CTerrainWaterShader : public CTexturedShader
+{
+public:
+	CTerrainWaterShader();
+	virtual ~CTerrainWaterShader();
+
+	virtual D3D12_INPUT_LAYOUT_DESC CreateInputLayout();
+
+	virtual D3D12_SHADER_BYTECODE CreateVertexShader(ID3DBlob** ppd3dShaderBlob);
+	virtual D3D12_SHADER_BYTECODE CreatePixelShader(ID3DBlob** ppd3dShaderBlob);
+
+	virtual void CreateShader(ID3D12Device* pd3dDevice, ID3D12RootSignature* pd3dGraphicsRootSignature);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+class CRippleWaterShader : public CTexturedShader
+{
+public:
+	CRippleWaterShader();
+	virtual ~CRippleWaterShader();
+
+	virtual D3D12_INPUT_LAYOUT_DESC CreateInputLayout();
+	virtual D3D12_RASTERIZER_DESC CreateRasterizerState();
+
+	virtual D3D12_SHADER_BYTECODE CreateVertexShader(ID3DBlob** ppd3dShaderBlob);
+	virtual D3D12_SHADER_BYTECODE CreatePixelShader(ID3DBlob** ppd3dShaderBlob);
+
+	virtual void CreateShader(ID3D12Device* pd3dDevice, ID3D12RootSignature* pd3dGraphicsRootSignature);
+};
