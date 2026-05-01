4043f319817cd0df9af273fc5a175216f001bc4f
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-05-02 00:06:21 +0900

    성 에셋 꾸밈

--- PracticeProject08-9-1/GameScene.cpp
+++ PracticeProject08-9-1/GameScene.cpp
@@ -305,7 +305,7 @@ namespace
 
 	static constexpr float kLocalPlayerRespawnDelay = 5.0f;
 
-	static constexpr ELocalStagePreset kLocalStagePreset = ELocalStagePreset::FullStage;
+	static constexpr ELocalStagePreset kLocalStagePreset = ELocalStagePreset::Test;
 
 	// -----------------------------------------------------------------------------
 	// HP
@@ -2768,7 +2768,7 @@ void CGameScene::BuildObjects(ID3D12Device* dev, ID3D12GraphicsCommandList* cmd)
 	const DXGI_FORMAT kDsvFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
 
 #ifndef USING_NETWORK
-	//BuildColliderBatch(dev, cmd, pColliderShader, kRTCount, rtvFormats, kDsvFormat);
+	BuildColliderBatch(dev, cmd, pColliderShader, kRTCount, rtvFormats, kDsvFormat);
 #endif
 
 	pTreeStaticShader->CreateShader(dev, m_pd3dGraphicsRootSignature.Get(), kRTCount, rtvFormats, kDsvFormat);
@@ -2817,7 +2817,7 @@ void CGameScene::BuildObjects(ID3D12Device* dev, ID3D12GraphicsCommandList* cmd)
 	
 #ifndef USING_NETWORK
 	//DumpStaticGridOccupancyLog();
-	//BuildStaticWorldSubmeshOOBBDebugObjects(dev, cmd);
+	BuildStaticWorldSubmeshOOBBDebugObjects(dev, cmd);
 #endif
 	BuildSkinnedBatch(dev, cmd, pSkinnedShader, kRTCount, rtvFormats, kDsvFormat);
 
