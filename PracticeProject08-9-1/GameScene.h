335c28a07dc034aab9e2d546863a78ee34c33bbb
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-05-24 02:04:44 +0900

    Revert "메모리 절약을 위한 GameScene 빌드 직후 메모리 검사"
    This reverts commit a8785b406aec57f7495ea29c9eac1b98d479b922.

--- PracticeProject08-9-1/GameScene.h
+++ PracticeProject08-9-1/GameScene.h
@@ -1329,15 +1329,14 @@ private:
     bool LoadStaticPlacementFile(const std::string& filePath);
 	bool LoadSceneCubeBoxColliderReport(const std::string& filePath);
 	bool ExportStaticWorldLocalOOBBReport(
-		const std::string& filePath,
-		const std::vector<size_t>& placementIndices,
-		const std::vector<CGameObject*>& objects
-		) const;
+	const std::string& filePath,
+	const std::vector<size_t>& placementIndices,
+	const std::vector<CGameObject*>& objects
+	) const;
 	void ResetStaticPlacementCounts();
 	void ReleaseBuildOnlySceneData();
-	void DumpBuildMemoryReport() const;
-	void ApplyStaticPlacementCounts();
-	static float QuaternionToYawDegrees(const XMFLOAT4& q);
+    void ApplyStaticPlacementCounts();
+    static float QuaternionToYawDegrees(const XMFLOAT4& q);
 
 private:
     std::vector<StaticPlacementEntry>   m_staticPlacementEntries;
