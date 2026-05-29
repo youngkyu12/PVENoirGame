7a1a49dd1a3b48afc4233ee6d3d0c27eab88d807
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-05-30 00:26:39 +0900

    인벤토리 칸 4개로 축소

--- PracticeProject08-9-1/GameSceneBuild.cpp
+++ PracticeProject08-9-1/GameSceneBuild.cpp
@@ -188,7 +188,7 @@ void CGameScene::BuildObjects(ID3D12Device* dev, ID3D12GraphicsCommandList* cmd)
 		}
 	}
 #else
-	m_localPlayerSlot = 1;
+	m_localPlayerSlot = 0;
 
 	const GameSceneStageFileSet& stageFiles = GetLocalStageFileSet(kLocalStagePreset);
 
