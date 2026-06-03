84a0b03a6017fa5218e7c703f639467f387775de
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-06-03 21:27:33 +0900

    터레인 정상화

--- PracticeProject08-9-1/GameSceneHelper.h
+++ PracticeProject08-9-1/GameSceneHelper.h
@@ -286,6 +286,5 @@ namespace GameSceneHelper
 	constexpr int kTerrainHeightMapSamples = 1025;
 	constexpr float kTerrainWorldSize = 1200.0f;
 	constexpr float kTerrainHorizontalScale = kTerrainWorldSize / static_cast< float >( kTerrainHeightMapSamples - 1 );
-	constexpr float kTerrainVerticalScale = 2.352941f;
-
+	constexpr float kTerrainVerticalScale = 0.0738f;
 }
